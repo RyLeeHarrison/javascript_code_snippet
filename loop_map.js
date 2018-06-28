aaf49c4c5608bbfe8de868fe1f81a5fd2bb8c089
RyLee Harrison <voxun@protonmail.com>
2018-06-28 05:44:26 -0500

    split loops by type

---
+++ loop_map.js
@@ -0,0 +1,18 @@
+const dummyMap = require('./dummy_data/dummyMap')
+
+function forEachGet(map) {
+  const lookup = (key) => map.get(key)
+
+  map.forEach((value, key) => {
+    Array.isArray(value)
+      ? console.log(lookup(key))
+      : console.log(key, value)
+  })
+}
+
+function forEach(map) {
+  map.forEach((value, key) => console.log(key, value))
+}
+
+forEachGet(dummyMap)
+forEach(dummyMap)
---
+++ loop_objects.js
@@ -0,0 +1,15 @@
+const dummyObject = require('./dummy_data/dummyObject')
+
+function forEachKeys(obj) {
+  Object.keys(obj)
+    .forEach((key, index) => console.log(key, obj[key]))
+}
+
+function forEntries(obj) {
+  for (const [key, value] of Object.entries(obj)) {
+    console.log(key, value)
+  }
+}
+
+forEachKeys(dummyObject)
+forEntries(dummyObject)
