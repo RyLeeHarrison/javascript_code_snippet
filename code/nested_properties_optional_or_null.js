4d7daae904f1c6358f18f1a6d150fb4652d1c360
RyLee Harrison <voxun@protonmail.com>
2018-07-30 14:26:04 -0500

    ++ & slowloris
    Working on a node.js slowloris  implementation as well as other amplification layer attacks...

--- code/nested_properties_optional_or_null.js
+++ code/nested_properties_optional_or_null.js
@@ -1,9 +1,14 @@
+// Just and example, DON'T use this.
+// Your colleagues will hate you.
+
 const top = {
 	a: {
 		b: "value"
 	}
 }
 
-if ({...{...top}.a}.b) {
+if ({ ...{ ...top
+		}.a
+	}.b) {
 	console.log(`satisfied: ${({...{...top}.a}).b}`)
 }
