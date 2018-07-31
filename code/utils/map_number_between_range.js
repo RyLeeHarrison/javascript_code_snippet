a9d1ff4f0edcf32b67ef06490253ba2f3294f691
RyLee Harrison <voxun@protonmail.com>
2018-07-30 20:33:53 -0500

    ++

---
+++ code/utils/map_number_between_range.js
@@ -0,0 +1,10 @@
+// map_value(value, fromLow, fromHigh, toLow, toHigh)
+
+const map_value = (x, in_min, in_max, out_min, out_max) => {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+const input = 10;
+const output = map_value(input, 0, 1023, 0, 255);
+
+console.log(output); // ==> 2.4926686217008798
