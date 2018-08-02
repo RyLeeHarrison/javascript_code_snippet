ba9cfa3ba65d4255041eddcd4258f664e744f7f8
RyLee Harrison <voxun@protonmail.com>
2018-08-01 21:26:38 -0500

    ++

---
+++ code/utils/fast_text_filter.js
@@ -0,0 +1,15 @@
+const uppercaseAlpha = text => [...(text)].filter(c => {
+    const d = c.charCodeAt(0);
+    return (d > 64 && d < 91)
+}).join("")
+
+const uglyInputText = uppercaseAlpha(`OP
+J()UY*T&*F^OYIVBUOINPd
+                hs8OU{(D&SUIA {D*(Stdus[aoudut sa7d
+
+asd as  d as d sada sy97t86fuyvibuoihpj908ysda d dasd sa d`
+);
+
+console.log( // ==> "OPJUYTFOYIVBUOINPOUDSUIADS"
+    uglyInputText
+)
