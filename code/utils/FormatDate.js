0fa3ea0e2bcef22ac7b5ea8ea864840ae3c86f0b
RyLee Harrison <voxun@protonmail.com>
2018-08-11 15:08:45 -0500

    ++

---
+++ code/utils/FormatDate.js
@@ -0,0 +1,22 @@
+class FormatDate extends Date {
+  constructor(dateStr) {
+    super(dateStr);
+  }
+
+  getFormattedDate() {
+    const days = [
+      'Mon', 'Tu', 'Wed', 'Th', 'Fri',
+      'Sat', 'Sun'];
+
+    const months = [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May',
+			'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+      'Nov', 'Dec'];
+
+    return `${days[this.getDay()]}, ${months[this.getMonth()]} ${this.getDate()} ${this.getFullYear()}`;
+  }
+}
+
+console.log( // => Sunday, Aug 11 2018
+  new FormatDate(new Date()).getFormattedDate()
+);
